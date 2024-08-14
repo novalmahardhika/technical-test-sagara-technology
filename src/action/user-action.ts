570d957b3f4d4f4543cb7cc13694a68b9f64bfa4
novalmahardhika <asubasa9@gmail.com>
2024-08-15 05:55:11 +0700

    fix: fix minor bug

--- src/action/user-action.ts
+++ src/action/user-action.ts
@@ -55,37 +55,3 @@ export async function deleteStudent(id: string) {
     return { error: 'Internal Server error' }
   }
 }
-
-// export async function updateStudent() {}
-
-// export async function deleteStudent(id: string) {
-//   try {
-//     const data: StudentType[] = await getStudent()
-//     const newData = data.filter((student) => student.id !== id)
-
-//     const originalFilePath = path.join(
-//       process.cwd(),
-//       'src',
-//       'temp',
-//       'store-data.json'
-//     )
-//     const tempFilePath = path.join('/tmp', 'store-data.json')
-
-//     // Copy the file to /tmp directory
-//     await fs.copyFile(originalFilePath, tempFilePath)
-
-//     // Check if the file exists in /tmp directory
-//     try {
-//       await fs.access(tempFilePath)
-//     } catch (error) {
-//       throw new Error(`File not found: ${tempFilePath}`)
-//     }
-
-//     await fs.writeFile(tempFilePath, JSON.stringify(newData, null, 2), 'utf-8')
-
-//     return { message: 'Student deleted successfully' }
-//   } catch (error) {
-//     console.error(error)
-//     return { message: 'Internal Server error' }
-//   }
-// }
