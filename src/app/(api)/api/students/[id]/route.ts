1e89387a3e60f685d3d86a24ec20c9572bceb82c
novalmahardhika <asubasa9@gmail.com>
2024-08-15 11:27:53 +0700

    fix: path directory

--- src/app/(api)/api/students/[id]/route.ts
+++ src/app/(api)/api/students/[id]/route.ts
@@ -12,23 +12,40 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   try {
+    // const data = await fs.readFile(filePath)
+
+    // console.log(params.id)
+
+    // const students: StudentType[] = JSON.parse(data.toString() || '[]')
+
+    // const newData = students.filter((student) => student.id !== params.id)
+
+    // // const tempFilePath = path.join(process.cwd(), '/tmp', 'store-data.json')
+
+    // // Write the updated data to the /tmp directory
+    // await fs.writeFile(filePath, JSON.stringify(newData, null, 2), 'utf-8')
+
+    // return Response.json(
+    //   { message: 'Success', data: JSON.parse(data.toString() || '[]') },
+    //   { status: 200 }
+    // )
+    // Check if the file exists
+    try {
+      await fs.access(filePath)
+    } catch (error) {
+      // If the file does not exist, create it with an empty array
+      await fs.writeFile(filePath, JSON.stringify([], null, 2), 'utf-8')
+    }
+
     const data = await fs.readFile(filePath)
-
-    console.log(params.id)
-
     const students: StudentType[] = JSON.parse(data.toString() || '[]')
 
     const newData = students.filter((student) => student.id !== params.id)
 
-    // const tempFilePath = path.join(process.cwd(), '/tmp', 'store-data.json')
-
     // Write the updated data to the /tmp directory
     await fs.writeFile(filePath, JSON.stringify(newData, null, 2), 'utf-8')
 
-    return Response.json(
-      { message: 'Success', data: JSON.parse(data.toString() || '[]') },
-      { status: 200 }
-    )
+    return Response.json({ message: 'Success', data: newData }, { status: 200 })
   } catch (error) {
     console.log(error)
     return new Response('Internal Server Error', { status: 500 })
